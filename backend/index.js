46ea43d8fdc9d9cabc7851eaaee5a17f1e280155
Abhay Singh <abhaysingh10032004@gmail.com>
2025-07-18 19:43:01 +0530

    Express json size increased

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use(cors({
 
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const authMiddleware = require('./middleware/authMiddleware');
